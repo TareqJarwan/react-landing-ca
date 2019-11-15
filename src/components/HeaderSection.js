c1c85dcb770de833b466e65e3d0ecab077e33370
TareqJarwan <tjarwan22@gmail.com>
2019-11-15 15:02:20 +0200

    sdasd

--- src/components/HeaderSection.js
+++ src/components/HeaderSection.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import classNames from "classnames";
-import {Link} from "react-router-dom";
+//import {Link} from "react-router-dom";
+import {Link, animateScroll as scroll} from "react-scroll";
 
 const HeaderSection = ({isSticky}) => {
+    const scrollToTop = () => {
+        scroll.scrollToTop();
+    };
     return (
         <header className={classNames("header_area animated", {"sticky slideInDown": isSticky},)}>
             <div className="container-fluid">
@@ -12,34 +16,111 @@ const HeaderSection = ({isSticky}) => {
                         <div className="menu_area">
                             <nav className="navbar navbar-expand-lg navbar-light">
                                 {/* Logo */}
-                                <Link className="navbar-brand text-light" to="#">Ca.</Link>
-                                <button className="navbar-toggler" type="button" data-toggle="collapse"
-                                        data-target="#ca-navbar" aria-controls="ca-navbar" aria-expanded="false"
-                                        aria-label="Toggle navigation"><span className="navbar-toggler-icon"/>
+                                <Link className="navbar-brand text-light"
+                                      onClick={scrollToTop}
+                                      spy={true}
+                                      smooth={true}
+                                      offset={-70}
+                                      duration={500}>Ca.</Link>
+                                <button className="navbar-toggler"
+                                        type="button"
+                                        data-toggle="collapse"
+                                        data-target="#ca-navbar"
+                                        aria-controls="ca-navbar"
+                                        aria-expanded="false"
+                                        aria-label="Toggle navigation">
+                                    <span className="navbar-toggler-icon"/>
                                 </button>
                                 {/* Menu Area */}
                                 <div className="collapse navbar-collapse" id="ca-navbar">
-                                    <ul className="navbar-nav ml-auto" id="nav">
-                                        <li className="nav-item active"><Link className="nav-link"
-                                                                              to="#home">Home</Link></li>
-                                        <li className="nav-item"><Link className="nav-link"
-                                                                       to="#about">About</Link>
+                                    <ul className="navbar-nav nav-items ml-auto" id="nav">
+                                        <li className="nav-item">
+                                            <Link activeClass="active"
+                                                  to="home"
+                                                  spy={true}
+                                                  smooth={true}
+                                                  offset={-70}
+                                                  duration={500}>
+                                                Home
+                                            </Link>
+                                            {/*<Link className="nav-link" to="#home">Home</Link>*/}
                                         </li>
-                                        <li className="nav-item"><Link className="nav-link"
-                                                                       to="#features">Features</Link></li>
-                                        <li className="nav-item"><Link className="nav-link"
-                                                                       to="#screenshot">Screenshot</Link></li>
-                                        <li className="nav-item"><Link className="nav-link"
-                                                                       to="#pricing">Pricing</Link>
+                                        <li className="nav-item">
+                                            <Link activeClass="active"
+                                                  to="about"
+                                                  spy={true}
+                                                  smooth={true}
+                                                  offset={-70}
+                                                  duration={500}>
+                                                About
+                                            </Link>
+                                            {/*<Link className="nav-link" to="#about">About</Link>*/}
                                         </li>
-                                        <li className="nav-item"><Link className="nav-link"
-                                                                       to="#testimonials">Testimonials</Link>
+                                        <li className="nav-item">
+                                            <Link activeClass="active"
+                                                  to="features"
+                                                  spy={true}
+                                                  smooth={true}
+                                                  offset={-70}
+                                                  duration={500}>
+                                                Features
+                                            </Link>
+                                            {/*<Link className="nav-link" to="#features">Features</Link>*/}
                                         </li>
-                                        <li className="nav-item"><Link className="nav-link"
-                                                                       to="#team">Team</Link>
+                                        <li className="nav-item">
+                                            <Link activeClass="active"
+                                                  to="screenshot"
+                                                  spy={true}
+                                                  smooth={true}
+                                                  offset={-70}
+                                                  duration={500}>
+                                                Screenshot
+                                            </Link>
+                                            {/*<Link className="nav-link" to="#screenshot">Screenshot</Link>*/}
                                         </li>
-                                        <li className="nav-item"><Link className="nav-link"
-                                                                       to="#contact">Contact</Link>
+                                        <li className="nav-item">
+                                            <Link activeClass="active"
+                                                  to="pricing"
+                                                  spy={true}
+                                                  smooth={true}
+                                                  offset={-70}
+                                                  duration={500}>
+                                                Pricing
+                                            </Link>
+                                            {/*<Link className="nav-link" to="#pricing">Pricing</Link>*/}
+                                        </li>
+                                        <li className="nav-item">
+                                            <Link activeClass="active"
+                                                  to="testimonials"
+                                                  spy={true}
+                                                  smooth={true}
+                                                  offset={-70}
+                                                  duration={500}>
+                                                Testimonials
+                                            </Link>
+                                            {/*<Link className="nav-link" to="#testimonials">Testimonials</Link>*/}
+                                        </li>
+                                        <li className="nav-item">
+                                            <Link activeClass="active"
+                                                  to="team"
+                                                  spy={true}
+                                                  smooth={true}
+                                                  offset={-70}
+                                                  duration={500}>
+                                                Team
+                                            </Link>
+                                            {/* <Link className="nav-link" to="#team">Team</Link>*/}
+                                        </li>
+                                        <li className="nav-item">
+                                            <Link activeClass="active"
+                                                  to="contact"
+                                                  spy={true}
+                                                  smooth={true}
+                                                  offset={-70}
+                                                  duration={500}>
+                                                Contact
+                                            </Link>
+                                            {/*<Link className="nav-link" to="#contact">Contact</Link>*/}
                                         </li>
                                     </ul>
                                     <div className="sing-up-button d-lg-none">
